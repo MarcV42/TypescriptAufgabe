7a3f760c2c6c8423f264da010e8fb8304c94a77a
MVe11111 <90624942+Sprudelflasch@users.noreply.github.com>
2023-09-25 16:42:54 +0200

    init

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import './style.css'
+
+let
+    age : number = 13,
+
+    username : string = "",
+    isAdmin: boolean = false;
+    const score: number = 1;
+
+
+for (let i : number = 0; i< age; i++){
+    console.log(i)
+}
+if (age >= 18){
+    console.log("is over 18")
+}
+else{
+    console.log("is underage")
+}
+
+console.log(age);
+
+if (score !== 0) {
+    console.log('Score ist verfügbar.');
+}
+
+
